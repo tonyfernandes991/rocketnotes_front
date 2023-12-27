92a06f8fb46ea5ac0540c582c62af0be0136ed11
tonyfernandes991 <112737052+tonyfernandes991@users.noreply.github.com>
2023-12-27 09:40:33 -0300

    rocketnotes front

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,37 @@
+import {Container, Profile, Logout} from './styles';
+import { useAuth} from '../../hooks/auth'
+import { RiShutDownLine } from 'react-icons/ri'
+import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
+
+export function Header(){
+    const { signOut, user } = useAuth();
+    const navigation = useNavigate()
+
+    function handleSignOut(){
+        navigation("/")
+        signOut()
+    }
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+    return(
+        <Container>
+            <Profile to='/profile'>
+                <img 
+                    src={avatarUrl} 
+                    alt={user.name}
+                />
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>{user.name}</strong>
+                </div>
+            </Profile>
+
+            <Logout onClick={handleSignOut}>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    )
+}
